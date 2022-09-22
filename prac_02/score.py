098d5be6da97ea0b2a7a63324703cb9fa500e4a6
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-09-22 18:02:06 +1000

    score.py updated

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,22 +1,33 @@
 """
-CP1404 | Practical 1: Debugging | Tahlia Clennar
-Broken program to determine score status
+CP1404 | Practical 2: Scores | Tahlia Clennar
+Program to determine score status and display random score
 """
+import random
 
 
 def main():
     """main function"""
     # getting user input
     score = float(input("Enter score: "))
+    print(get_score(score))
+
+    # getting random score
+    random_score = random.randint(0, 100)
+    print("Random Score: {}". format(random_score))
+    print(get_score(random_score))
+
+
+def get_score(score):
+    """Function to take user's score and print result"""
     # conditional logic for user score
     if score > 100 or score < 0:
-        print("Invalid score")
+        return "Invalid score"
     elif score >= 90:
-        print("Excellent")
+        return "Excellent"
     elif score >= 50:
-        print("Passable")
+        return "Passable"
     else:
-        print("Bad")
+        return "Bad"
 
 
 main()
