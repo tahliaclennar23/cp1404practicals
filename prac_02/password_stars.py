83a47b3b019318ca8a0a25a8abc732d282c86f5b
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-09-22 16:40:21 +1000

    Adding main()

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,18 +2,26 @@
 CP1404 | Practical 02: Menus | Tahlia Clennar
 Program to ...
 """
-# getting user input
-password = input("Enter Password: ")
 
-# setting minimum password length
-minimum_password_length = 8
 
-# error checking
-while len(password) < minimum_password_length:
-    print("Password must be at least {} characters". format(minimum_password_length))
+def main():
+    """main function"""
 
-    # getting user input again
+    # getting user input
     password = input("Enter Password: ")
 
-# printing asterisks as long as word
-print('*' * len(password))
+    # setting minimum password length
+    minimum_password_length = 8
+
+    # error checking
+    while len(password) < minimum_password_length:
+        print("Password must be at least {} characters".format(minimum_password_length))
+
+        # getting user input again
+        password = input("Enter Password: ")
+
+    # printing asterisks as long as word
+    print('*' * len(password))
+
+
+main()
