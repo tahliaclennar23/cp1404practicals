7d7ab9a4e1de0d2ad5a9e1aab724234b3345b584
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-09-22 17:27:44 +1000

    Adding files from Prac 1 for temperatures.py and score.py

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,14 +3,20 @@ CP1404 | Practical 1: Debugging | Tahlia Clennar
 Broken program to determine score status
 """
 
-# getting user input
-score = float(input("Enter score: "))
-# conditional logic for user score
-if score > 100 or score < 0:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    """main function"""
+    # getting user input
+    score = float(input("Enter score: "))
+    # conditional logic for user score
+    if score > 100 or score < 0:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+main()
