ea69ce2b2639d2ab4f50dad417891eee672362fe
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-10-02 21:16:33 +1000

    Add user name input file to files.py

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,3 +3,11 @@ Tahlia Clennar
 CP1404 Practical 03- Files
 Program to read and write to files
 """
+FILE_NAME = "name.txt"
+with open(FILE_NAME, 'w') as name_txt:
+    name = input("Enter your name: ")
+    print(f"{name}", file=name_txt)
+with open(FILE_NAME, 'r') as name_txt:
+    text = name_txt.read()
+    print(f"Your name is {text}")
+
