527440ac74d8571b8fe0c621dc99ce940ba379e0
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-10-07 12:46:17 +1000

    Get username and grant or deny access for list_excercises.py program

--- prac_04/list_excercises.py
+++ prac_04/list_excercises.py
@@ -11,3 +11,8 @@ print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
 
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Enter username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
