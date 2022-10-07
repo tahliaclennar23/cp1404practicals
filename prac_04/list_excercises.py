ed1fee618018f86639ad604b6a0ced59aa89e939
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-10-07 12:44:29 +1000

    Add usernames for list_excercises.py program

--- prac_04/list_excercises.py
+++ prac_04/list_excercises.py
@@ -8,3 +8,6 @@ print(f"The last number is {numbers[4]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
