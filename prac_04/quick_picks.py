31c3aa6571898bd43fc6673c4f7faa8bafe1f887
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-10-12 08:33:42 +1000

    Fill quick pick lists with 6 random numbers

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,13 +1,18 @@
 """
 
 """
-
+import random
 
 LENGTH_QUICK_PICK = 6
+QUICK_PICK_MAXIMUM = 45
+QUICK_PICK_MINIMUM = 1
+
 
 quick_picks = int(input("How many quick picks?: "))
 
 for i in range(quick_picks):
     quick_pick = []
-
-
+    for j in range(LENGTH_QUICK_PICK):
+        random_number = random.randint(QUICK_PICK_MINIMUM, QUICK_PICK_MAXIMUM)
+        quick_pick.append(random_number)
+    print(quick_pick)
