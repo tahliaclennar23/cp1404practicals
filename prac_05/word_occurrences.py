04ffc564b36e0ac54e4ad840c8a42203d3398ca0
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-10-19 09:16:24 +1000

    Get text and occurrence of words for word_occurrences.py

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,3 +1,9 @@
+"""
+Tahlia Clennar
+CP1404 Practical 05- Word Occurrences
+Program to count the occurrences of words in a string.
+"""
+
 user_text = input("Enter text: ")
 print(f"Text: {user_text}")
 word_to_count = {}
