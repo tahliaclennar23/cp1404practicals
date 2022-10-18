b4d20b3727393a7ac3f8736fd937922a3ed6654e
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-10-19 08:29:36 +1000

    Update state_names.py to get lowercase inputs and format string.

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,14 +5,16 @@ State names in a dictionary
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
-            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    try:
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:{3}} is {name}")
