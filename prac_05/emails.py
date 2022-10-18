5bab1d4538edc74f4f273ba0d96dedcee2f6f234
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-10-19 09:47:30 +1000

    Initial commit

---
+++ prac_05/emails.py
@@ -0,0 +1,9 @@
+"""
+Tahlia Clennar
+CP1404 Practical 05- Emails
+Program to store user's emails and names in a dictionary.
+Estimate:45 minutes
+
+"""
+email = input("Enter email: ")
+while email != "":
