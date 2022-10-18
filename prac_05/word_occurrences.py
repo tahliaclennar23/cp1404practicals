43766f8f33bbe8981f0ab14c1d7d6509dd5942a4
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-10-19 09:29:03 +1000

    Align outputs correctly for word_occurrences.py

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,6 +2,7 @@
 Tahlia Clennar
 CP1404 Practical 05- Word Occurrences
 Program to count the occurrences of words in a string.
+Estimate:25 minutes
 """
 
 user_text = input("Enter text: ")
@@ -10,6 +11,6 @@ word_to_count = {}
 
 for word in user_text.split(" "):
     word_to_count[word] = word_to_count.get(word, 0) + 1
-
+max_length = max(len(word) for word in list(word_to_count.keys()))
 for word in sorted(word_to_count):
-    print(f"{word} : {word_to_count[word]}")
+    print(f"{word:{max_length}} : {word_to_count[word]}")
