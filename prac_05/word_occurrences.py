bb64e12563a3516e2daa9e3450bb5a6818c1d957
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-10-19 09:21:50 +1000

    Sort words

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -11,5 +11,5 @@ word_to_count = {}
 for word in user_text.split(" "):
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
-for word in word_to_count:
+for word in sorted(word_to_count):
     print(f"{word} : {word_to_count[word]}")
