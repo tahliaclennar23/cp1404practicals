018aaab913a636f17deaebfed7dfcc4f920f572e
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-10-19 17:59:46 +1000

    Print correct name for extracted name from emails for emails.py

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,14 +3,34 @@ Tahlia Clennar
 CP1404 Practical 05- Emails
 Program to store user's emails and names in a dictionary.
 Estimate:45 minutes
+Actual time: 1 hour (with breaks)
 
 """
-email = input("Enter email: ")
-name_email_list = []
-while email != "":
+
+
+def main():
+    """ Get email as input and extract name"""
+    email = input("Enter email: ")
+    email_to_name = {}
+    while email != "":
+        name = extract_name(email)
+        email_to_name[email] = name
+        correct_name = input(f"Is your name {name}? (Y/n): ")
+        if correct_name.upper() != 'Y' and correct_name.upper() != '':
+            name = input("Name: ")
+            email_to_name[email] = name
+        email = input("Enter email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def extract_name(email):
+    """Extract name in proper format"""
     name_email_list = email.split("@")
     name = name_email_list[0].title()
     name = name.replace(".", " ")
-    print(name)
-    email = input("Enter email: ")
+    return name
 
+
+main()
