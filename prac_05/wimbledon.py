a417572ea3fa1e0a8ce5077149bb5d4a39f14e00
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-10-19 18:27:33 +1000

    Refactor wimbledon records

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -2,12 +2,17 @@ FILENAME = "wimbledon.csv"
 
 
 def main():
+    wimbledon_records = get_wimbledon_records(FILENAME)
+    print(wimbledon_records)
+
+
+def get_wimbledon_records(filename):
     wimbledon_records = []
-    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
         for line in in_file:
-            wimbledon_records.append(line)
-        print(wimbledon_records)
+            wimbledon_records.append(line.split(","))
+        return wimbledon_records
 
 
 main()
