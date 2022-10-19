f2e04124bf1f8f800ffd38fa1fe061747392364c
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-10-19 18:23:05 +1000

    Print wimbledon records

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,2 +1,13 @@
-FILENAME= "wimbledon.csv"
-with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+FILENAME = "wimbledon.csv"
+
+
+def main():
+    wimbledon_records = []
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            wimbledon_records.append(line)
+        print(wimbledon_records)
+
+
+main()
