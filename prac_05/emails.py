57a726266d44cb49aac9f9fb5fb4ee6b6c936589
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-10-19 11:13:49 +1000

    Get first and last name from email for emails.py program

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,4 +6,11 @@ Estimate:45 minutes
 
 """
 email = input("Enter email: ")
+name_email_list = []
 while email != "":
+    name_email_list = email.split("@")
+    name = name_email_list[0].title()
+    name = name.replace(".", " ")
+    print(name)
+    email = input("Enter email: ")
+
