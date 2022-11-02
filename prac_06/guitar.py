044aedb10ce6034e7a945b47031c7c2901c1f572
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-11-02 17:07:40 +1000

    Get guitar details and age of guitar

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,8 +1,20 @@
+YEAR = 2022
+
+
 class Guitar:
     """Represent programming language."""
 
-    def __init__(self, name="", cost=0, year=0):
+    def __init__(self, name="", year=0, cost=0):
         """Initialise values"""
         self.name = name
         self.cost = cost
         self.year = year
+
+    def __str__(self):
+        """Return guitar details"""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Return age of guitar"""
+        return YEAR - self.year
+
