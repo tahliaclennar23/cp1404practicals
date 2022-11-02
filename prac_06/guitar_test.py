b1529233252538c38f357958f3897468d48ee928
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-11-02 17:17:54 +1000

    Add guitar year, name and cost.

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -3,3 +3,11 @@ Tahlia Clennar
 Estimated time: 15 minutes
 Actual time:  minutes
 """
+from prac_06.guitar import Guitar
+
+
+def run_guitar_tests():
+    """Test previous guitar class"""
+    name = "Gibson L-5 CES"
+    year = 1922
+    cost = 16035.4
