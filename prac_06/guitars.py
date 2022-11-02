1f3d894c3f460d78c0222091da11f30c8267d1ca
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-11-02 17:49:06 +1000

    Add vintage string and print guitar details.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -15,6 +15,18 @@ def main():
         year = int(input("Year: "))
         cost = float(input("Cost: "))
         print(f"{name}({year}) : {cost:,.2f} added.")
-        guitar = Guitar(name, year,cost)
+        guitar = Guitar(name, year, cost)
         guitars.append(guitar)
         name = input("Name: ")
+    print("These are my guitars:")
+    if guitars:
+        max_guitar_name_length = max(len(guitar.name) for guitar in guitars)
+        for i, guitar in enumerate(guitars, 1):
+            vintage = "(vintage)" if guitar.is_vintage() else ""
+            print(f"Guitar {i}: {guitar.name:>{max_guitar_name_length}} ({guitar.year}),"
+                  f"worth ${guitar.cost:10,.2f}{vintage}")
+    else:
+        print("No guitars")
+
+
+main()
