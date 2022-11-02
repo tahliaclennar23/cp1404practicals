f6657d369cb3ea047a2b1e96656ef32594b42fee
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-11-02 16:54:04 +1000

    Add __str__ method to the class.

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -14,3 +14,7 @@ class ProgrammingLanguage:
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def __str__(self):
+        """Provide coding information"""
+        return f"{self.name}, {self.typing} Typing, Reflection= {self.reflection}, First appeared in {self.year}"
