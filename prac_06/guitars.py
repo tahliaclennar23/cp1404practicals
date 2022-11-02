253c5f6e90f76a6d48cd3b21b7ff47e46889b98d
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-11-02 17:39:56 +1000

    Get year as cost as inputs.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -5,7 +5,13 @@ Actual time:   minutes
 """
 
 from prac_06.guitar import Guitar
+
+
 def main():
-    guitars=[]
+    guitars = []
     print("My guitars!")
     name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        print(f"{name}({year}) : {cost:,.2f} added.")
