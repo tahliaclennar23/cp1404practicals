9113c83418fb3ebd7e6cbd84b6c67fa4622a4af3
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-11-02 16:48:51 +1000

    Add init function and initialise values.

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -3,3 +3,14 @@ Tahlia Clennar
 Estimated time: 30 minutes
 Actual time:
 """
+
+
+class ProgrammingLanguage:
+    """Represent programming language."""
+
+    def __init__(self, name="", typing="", reflection=False, year=0):
+        """Initialise values"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
