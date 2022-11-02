244b12d64c4122359e92a23fe43e0e90b0251903
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-11-02 17:03:40 +1000

    Add class and initialise values.

---
+++ prac_06/guitar.py
@@ -0,0 +1,8 @@
+class Guitar:
+    """Represent programming language."""
+
+    def __init__(self, name="", cost=0, year=0):
+        """Initialise values"""
+        self.name = name
+        self.cost = cost
+        self.year = year
