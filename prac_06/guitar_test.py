40f798e14c6c24009bb044ef20448d244ae511a9
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-11-02 17:27:02 +1000

    Add print statements.

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -3,6 +3,7 @@ Tahlia Clennar
 Estimated time: 15 minutes
 Actual time:  minutes
 """
+YEAR = 2022
 from prac_06.guitar import Guitar
 
 
@@ -14,3 +15,7 @@ def run_guitar_tests():
 
     guitar = Guitar(name, year, cost)
     another_guitar = Guitar("Another guitar", 2013, 5000)
+    print(f"{guitar.name} get_age() - Expected {YEAR - year}. Got {YEAR - guitar.year}")
+    print(f"{another_guitar.name} get_age ()- Expected {YEAR - 2000}. Got {YEAR - another_guitar.year}")
+    print(f"{guitar.name} is_vintage()- Expected {True}. Got {guitar.is_vintage()}")
+    print(f"{another_guitar.name} is_vintage() - Expected {False}. Got {another_guitar.is_vintage()}")
