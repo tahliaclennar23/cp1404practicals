31cee4e50c39e156fdf30eab0fc89a86bcdedb3c
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-11-02 16:57:20 +1000

    Create list containing three existing ProgrammingLanguage objects.

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -5,3 +5,7 @@ def main():
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    programming_languages = [python, ruby, visual_basic]
+    print("The dynamically typed languages are:")
+
+
