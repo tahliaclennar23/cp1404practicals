6470a55227b73b605aff3441ce11a24536fe59a1
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-11-14 18:56:23 +1000

    Update convert_miles_km.py file.

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,29 +1,41 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 __author__ = 'Tahlia Clennar'
 
+CONVERSION = 1.61
+
 
 class UnitConversionApp(App):
+    """Convert miles to kilometers in GUI"""
+    kilometers = StringProperty()
 
     def build(self):
         """ build the Kivy app from the kv file """
-        Window.size = (200, 100)
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def handle_calculate(self, value):
         """ handle calculation (could be button press or other call), output result to label widget """
+        miles = self.convert_to_number(value)
+        self.kilometers = str(miles * CONVERSION)
+
+    def handle_increment(self, value, increment):
+        miles = self.convert_to_number(value) + increment
+        self.root.ids.input_number.text = str(miles)
+
+    def convert_to_number(self, string):
+        """Convert string to number"""
         try:
-            result = float(value) * 1.61
-            self.root.ids.output_label.text = str(result)
+            number = float(string)
         except ValueError:
-            pass
-    # def handle_increment(self, value):
-    #     try:
-    # # handle_increment(input_text.text,-1)
+            number = 0.0
+        return number
 
 
 UnitConversionApp().run()
