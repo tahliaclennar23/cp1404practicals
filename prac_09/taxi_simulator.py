f8963417574d9554baec0faeaa368d1f8f56790d
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-11-23 12:24:53 +1000

    Add error handling for choosing taxi.

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -11,14 +11,22 @@ MENU = """q(uit), c(hoose) taxi, d(rive)"""
 
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
     print("Let's drive!")
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
-            print("Taxis available:")
+            print("Taxis available: ")
             for i, taxi in enumerate(taxis):
                 print(f"{i} - {taxi}")
+            taxi_choice = int(input("Choose taxi: "))
+            try:
+                current_taxi = taxis[taxi_choice]
+            except IndexError:
+                print("Invalid taxi choice")
+
         choice = input(">>> ").upper()
+
 
 main()
