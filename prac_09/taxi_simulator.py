14fe262ce7ea26e5589fe108e924ddba0bb8e896
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-11-23 12:20:07 +1000

    Display taxi's available after user hits "c".

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -9,3 +9,16 @@ from prac_09.taxi import Taxi
 MENU = """q(uit), c(hoose) taxi, d(rive)"""
 
 
+def main():
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    print("Let's drive!")
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            print("Taxis available:")
+            for i, taxi in enumerate(taxis):
+                print(f"{i} - {taxi}")
+        choice = input(">>> ").upper()
+
+main()
