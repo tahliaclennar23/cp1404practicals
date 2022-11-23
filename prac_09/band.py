c0a2f6e721ce0df451897242eb5a15b8666af757
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-11-23 11:57:57 +1000

    Create band class, __init__ function and __str__ function

--- prac_09/band.py
+++ prac_09/band.py
@@ -7,3 +7,12 @@ Tahlia Clennar
 
 class Band:
     """Create band class"""
+
+    def __init__(self, name=""):
+        """Initialise a band class instance."""
+        self.name = name
+        self.musicians = []
+
+    def __str__(self):
+        """Return string with musicians"""
+        return f"{self.name} ({','.join([musician.__str__() for musician in self.musicians])}"
