0c53a6f8cc99bd2edaf9f6d1b4dcd6ce7224b577
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-11-23 12:33:24 +1000

    Allow user to choose how far to drive.

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -12,6 +12,7 @@ MENU = """q(uit), c(hoose) taxi, d(rive)"""
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     current_taxi = None
+    total_cost = 0
     print("Let's drive!")
     print(MENU)
     choice = input(">>> ").upper()
@@ -25,8 +26,16 @@ def main():
                 current_taxi = taxis[taxi_choice]
             except IndexError:
                 print("Invalid taxi choice")
-
+        elif choice == "D":
+            if current_taxi:
+                current_taxi.start_fare()
+                travel_distance = float(input("Drive how far? "))
+                current_taxi.drive(travel_distance)
+                cost = current_taxi.get_fare()
+                total_cost += cost
+                print(f"Your {current_taxi.name} trip cost you ${cost:.2f}")
+            else:
+                print("You need to choose a taxi before you can drive")
         choice = input(">>> ").upper()
-
 
 main()
