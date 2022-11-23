5dbbad0e570db107e1ec3b288e25e85f9a2593d5
Tahlia Clennar <tahliaclennar23@gmail.com>
2022-11-23 12:11:00 +1000

    Create menu and import classes.

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,11 @@
+"""
+CP1404 Practical 9
+Taxi simulator
+Tahlia Clennar
+"""
+from prac_09.silver_service_taxi import SilverServiceTaxi
+from prac_09.taxi import Taxi
+
+MENU = """q(uit), c(hoose) taxi, d(rive)"""
+
+
